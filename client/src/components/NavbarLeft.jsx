8e8de6416891e1cc780e2dd4e6f3d03d6caf859f
naayifmuhammad <muhdnaayif@gmail.com>
2024-09-15 21:22:44 +0530

    Only admin and manager can add new user

--- client/src/components/NavbarLeft.jsx
+++ client/src/components/NavbarLeft.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import {React, useState, useEffect} from 'react';
 import '../styles/NavbarLeft.css';
 import { Link } from 'react-router-dom';
 
 const NavbarLeft = () => {
+  
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser)); // Parse the user object from localStorage
+    }
+  }, []);
+
+  function isAdmin(){
+    return user.user_type === "Admin"? true : false 
+  }
+  function isManager(){
+    return user.user_type === "Manager"? true : false 
+  }
+  function isStaff(){
+    return user.user_type === "Staff"? true : false 
+  }
+  
+
   return (
     <div className="navbar-left">
       <ul>
         <li><Link to="/dashboard">Dashboard</Link></li>
         <li><Link to="/login">Login</Link></li>
-        <li><Link to="/add-employee">Register Employee</Link></li>
+        {/* Conditional rendering for Register Employee link */}
+        {(isAdmin() || isManager()) && (
+          <li><Link to="/add-employee">Register Employee</Link></li>
+        )}
         <li><Link to="/complaint-reg">Complaint Registration</Link></li>
         <li><Link to="/views-complaints">View Complaint</Link></li>
       </ul>
